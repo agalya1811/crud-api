a45f4f5420d7c4c23045f07ee952e1054180a301
Agalya M <akalyakannan8872@gmail.com>
2025-05-08 14:12:06 +0530

    change

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
-const express = require('express');
+import express from "express";
+import movieRoutes from "./routes/movies.routes";
+
+
 const app = express();
 
 app.get('/',(req,res)=>{
     res.json({msg:"Hello World"});
 });
-//GET METHOD
-app.get('/movies',()=>{
 
-});
-
-//POST METHOD
-app.post('/movies',()=>{
-
-});
-
-//UPDATE METHOD
-app.put('/movies/:id',()=>{
-
-});
-
-//DELETE METHOD
-app.delete('/movies/:id',()=>{
-
-});
-
+app.use('/movies',movieRoutes);
 app.listen(8000,()=>{
     console.log(`Server is running at port 8000`);
     
---
+++ routes/movies.routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+const router = express.Router();
+
+
+//GET METHOD
+router.get('/movies',()=>{
+
+});
+
+//POST METHOD
+router.post('/movies',()=>{
+
+});
+
+//UPDATE METHOD
+router.put('/movies/:id',()=>{
+
+});
+
+//DELETE METHOD
+router.delete('/movies/:id',()=>{
+
+});
+
+export default router;
