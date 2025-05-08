cacaa05a69e0e63bce9d3488f9d3c6412b718de7
Agalya M <akalyakannan8872@gmail.com>
2025-05-08 13:05:42 +0530

    change

--- main.js
+++ main.js
@@ -4,8 +4,17 @@ const app = express();
 app.get('/',(req,res)=>{
     res.json({msg:"Hello World"});
 });
+//GET METHOD
+app.get('/movies',()=>{
+
+});
+
+//POST METHOD
+app.post('/movies',()=>{
+
+});
 
 app.listen(8000,()=>{
     console.log(`Server is running at port 8000`);
     
-});
+}); 
